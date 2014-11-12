79f2bfb35d139438eadf8968c84b5427a48f08cd
zetowp <zetowp@gmail.com>
2014-11-11 21:16:50 -0600

    green - hacemos que el tres pase

--- src/com/jzoft/katas/factoresprimos/FactoresPrimos.java
+++ src/com/jzoft/katas/factoresprimos/FactoresPrimos.java
@@ -7,8 +7,10 @@ public class FactoresPrimos {
 
 	public List<Integer> buscarFactoresPrimos(int numero) {
 		ArrayList<Integer> listaDeFactores = new ArrayList<Integer>();
-		if (numero > 1) {
+		if (numero % 2 == 0) {
 			listaDeFactores.add(2);
+		} else if (numero % 3 == 0) {
+			listaDeFactores.add(3);
 		}
 		return listaDeFactores;
 	}
