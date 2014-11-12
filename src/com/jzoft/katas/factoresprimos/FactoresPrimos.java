f9a5f61f17e4bf12b708bd0a337cc4c1af43a2fd
zetowp <zetowp@gmail.com>
2014-11-11 21:00:16 -0600

    green - que pase regresando lista vacia

--- src/com/jzoft/katas/factoresprimos/FactoresPrimos.java
+++ src/com/jzoft/katas/factoresprimos/FactoresPrimos.java
@@ -1,11 +1,12 @@
 package com.jzoft.katas.factoresprimos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FactoresPrimos {
 
 	public List<Integer> buscarFactoresPrimos(int i) {
-		return null;
+		return new ArrayList<Integer>();
 	}
 
 }
