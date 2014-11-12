935c8d0770f2f4cdf3e198802f2bde7ca8b37708
zetowp <zetowp@gmail.com>
2014-11-11 21:13:49 -0600

    red - caso para el tres fallando

--- test/com/jzoft/katas/factoresprimos/FactoresPrimosTest.java
+++ test/com/jzoft/katas/factoresprimos/FactoresPrimosTest.java
@@ -8,25 +8,33 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class FactoresPrimosTest {
-	
+
 	private FactoresPrimos factorador;
 
 	@Before
 	public void setUp() throws Exception {
 		this.factorador = new FactoresPrimos();
 	}
-	
+
 	@Test
 	public void dadoUno_regresaVacio() throws Exception {
 		List<Integer> factores = factorador.buscarFactoresPrimos(1);
 		assertEquals(0, factores.size());
 	}
-	
+
 	@Test
 	public void dadoDos_regresaListaConSoloDos() throws Exception {
 		List<Integer> factores = factorador.buscarFactoresPrimos(2);
 		assertEquals(1, factores.size());
 		assertEquals(2, factores.get(0).byteValue());
+	}
+
+	@Test
+	public void dadoTres_regresaListaConSoloTres() throws Exception {
+		List<Integer> factores = factorador.buscarFactoresPrimos(3);
+		assertEquals(1, factores.size());
+		assertEquals(3, factores.get(0).byteValue());
+
 	}
 
 }
